a6e14422b3be5f421ab78a78ae3111c105e6d710
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-12-01 02:04:39 +0300

    edit: added checking whether user in the db test
    Took 1 hour 41 minutes

--- src/test/java/edu/keepeasy/testcode/TestCodeApplicationTests.java
+++ src/test/java/edu/keepeasy/testcode/TestCodeApplicationTests.java
@@ -1,31 +1,88 @@
 package edu.keepeasy.testcode;
 
 import edu.keepeasy.testcode.domain.Message;
+import edu.keepeasy.testcode.domain.User;
 import edu.keepeasy.testcode.repo.MessageRepository;
 import edu.keepeasy.testcode.repo.UserRepository;
 import edu.keepeasy.testcode.service.Checker;
 import org.junit.Assert;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.sql.DataSource;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Properties;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class TestCodeApplicationTests {
-    @Autowired
-    UserRepository userRepository;
+    static Connection connection;
     @Autowired
     DataSource dataSource;
     @Autowired
+    UserRepository userRepository;
+    @Autowired
     MessageRepository messageRepository;
+
+    @BeforeAll
+    public static void init() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            Properties properties = new Properties();
+            properties.load(new FileReader(new File("src/main/resources/application.properties")));
+            connection = DriverManager.getConnection(
+                    properties.getProperty("jdbc.test.url"),
+                    properties.getProperty("spring.datasource.username"),
+                    properties.getProperty("spring.datasource.password"));
+            PreparedStatement set_foreign_0 = connection.prepareStatement("SET FOREIGN_KEY_CHECKS=0");
+            PreparedStatement truncate_table_message = connection.prepareStatement("TRUNCATE TABLE message");
+            PreparedStatement truncate_table_user = connection.prepareStatement("TRUNCATE TABLE user");
+            PreparedStatement truncate_table_user_role = connection.prepareStatement("TRUNCATE TABLE user_role");
+            PreparedStatement insertUser = connection.prepareStatement(
+                    "INSERT user(id,active,password,username) value (1,1,'qwerty123456','sirojiddin')");
+            PreparedStatement insertUserRole = connection.prepareStatement("INSERT user_role value (1,'USER')");
+            PreparedStatement set_foreign_1 = connection.prepareStatement("SET FOREIGN_KEY_CHECKS=1");
+            set_foreign_0.executeUpdate();
+            truncate_table_message.executeUpdate();
+            truncate_table_user_role.executeUpdate();
+            truncate_table_user.executeUpdate();
+            insertUser.executeUpdate();
+            insertUserRole.executeUpdate();
+            set_foreign_1.executeUpdate();
+        } catch (SQLException | ClassNotFoundException | IllegalAccessException | NoSuchMethodException |
+                InvocationTargetException | InstantiationException | IOException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    @AfterAll
+    public static void destruct() {
+        try {
+            PreparedStatement set_foreign_0 = connection.prepareStatement("SET FOREIGN_KEY_CHECKS=0");
+            PreparedStatement truncate_table_message = connection.prepareStatement("TRUNCATE TABLE message");
+            PreparedStatement truncate_table_user = connection.prepareStatement("TRUNCATE TABLE user");
+            PreparedStatement truncate_table_user_role = connection.prepareStatement("TRUNCATE TABLE user_role");
+            PreparedStatement set_foreign_1 = connection.prepareStatement("SET FOREIGN_KEY_CHECKS=1");
+            set_foreign_0.executeUpdate();
+            truncate_table_message.executeUpdate();
+            truncate_table_user.executeUpdate();
+            truncate_table_user_role.executeUpdate();
+            set_foreign_1.executeUpdate();
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
 
     @Test
     @Order(1)
@@ -65,7 +122,17 @@ class TestCodeApplicationTests {
             Assert.assertEquals("Кратко о том, зачем этот зверь нам нужен?",
                     message.getText());
         }
+    }
 
+    @Test
+    @Order(5)
+    public void isUserInDB() {
+        User sirojiddin = userRepository.findByUsername("sirojiddin");
+        Assert.assertNotNull(sirojiddin);
+        Assert.assertEquals("qwerty123456", sirojiddin.getPassword());
+
+        User notExists = userRepository.findByUsername("qwertyuiop");
+        Assert.assertNull(notExists);
     }
 
 
